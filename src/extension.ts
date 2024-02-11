bf43db438003c241f76a46e1229c725f52b340e5
eSarmir <e.sarmir@gmail.com>
2024-02-11 14:15:21 +0100

    Reads stored language levels on extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,19 @@
 import * as vscode from 'vscode';
-import { LanguageLevel } from './languageLevel';
-import { ExpcodeDataProvider } from './expcodeDataProvider';
 import { registerCommands } from './commands';
 import { registerEvents } from './events';
+import { getLanguageLevels } from './extensionState';
+import { ExpcodeDataProvider } from './expcodeDataProvider';
+import { LanguageLevel } from './languageLevel';
+import { get } from 'http';
 
 export async function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "expcode" is now active!');
-	
 	const dataProvider = new ExpcodeDataProvider();
 	vscode.window.registerTreeDataProvider('expcode', dataProvider);
 
-	const languages = await vscode.languages.getLanguages();
+	const languageLevels = getLanguageLevels(context);
 
-	const languageLevels = languages.map((language) => {
-		return new LanguageLevel(language);
-	});
-
-	registerEvents(languageLevels);
+	registerEvents(context, languageLevels);
 
 	context.subscriptions.push(
 		...registerCommands(languageLevels)
