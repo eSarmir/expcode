0d91d327873c9a857855ed8394cb9fb81b942c9c
eSarmir <e.sarmir@gmail.com>
2024-02-11 20:23:30 +0100

    Extracts viewContainer id into constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
 // Global state store IDs
 export const LanguageLevelsGlobalStateId = `expcode-language-levels`;
 
+// Views
+export const ExpcodeViewContainerId = 'expcode-view-container';
+
 // Commands
 export const ShowLanguageOfActiveEditorCommand = 'expcode.showLanguageOfActiveEditor';
 export const ShowLevelForLanguageCommand = 'expcode.showLanguageOfActiveEditor';
