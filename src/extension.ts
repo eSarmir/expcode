2aac2a3542d527252018600e3d19bd8cb48ce78a
eSarmir <e.sarmir@gmail.com>
2024-03-31 12:23:43 +0200

    Uses experience calculator outside of tests

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,15 @@ import { registerEvents } from './events';
 import { getLanguageLevels } from './extensionState';
 import { ExpcodeTreeDataProvider } from './expcodeTreeDataProvider';
 import { ExpcodeViewContainerId } from './constants';
+import { ExperienceCalculator } from './experienceCalculator';
 
 export async function activate(context: vscode.ExtensionContext) {
 	
 	const languageLevels = getLanguageLevels(context);
 	
 	const dataProvider = new ExpcodeTreeDataProvider(languageLevels);
+
+	const experienceCalculator = new ExperienceCalculator();
 
 	vscode.window.registerTreeDataProvider(
 		ExpcodeViewContainerId,
@@ -18,7 +21,8 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	registerEvents(
 		context, 
-		languageLevels, 
+		languageLevels,
+		experienceCalculator, 
 		dataProvider.refresh.bind(dataProvider));
 
 	context.subscriptions.push(
