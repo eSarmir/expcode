81a53ce0df44e4a56346d6a62f9041722f3fc5b8
eSarmir <e.sarmir@gmail.com>
2024-05-03 21:37:52 +0200

    Introduces language levels class

---
+++ src/languageLevels.ts
@@ -0,0 +1,42 @@
+import { LanguageLevel } from "./languageLevel";
+
+export class LanguageLevels {
+    private languageLevels: LanguageLevel[];
+
+    constructor(languageLevels: LanguageLevel[] = []) {
+        this.languageLevels = languageLevels;
+
+        var overallLanguageLevel = this.languageLevels.find((languageLevel) => languageLevel.getLanguageId() === 'overall');
+    
+        if (overallLanguageLevel === undefined) 
+        {
+            this.addOverallLevel();
+        }
+    }
+
+    public getLanguageLevels(): LanguageLevel[] {
+        return this.languageLevels;
+    }
+
+    public getLanguageLevel(languageId: string): LanguageLevel | undefined {
+        return this.languageLevels.find((languageLevel) => languageLevel.getLanguageId() === languageId);
+    }
+
+    public addLanguageLevel(languageLevel: LanguageLevel) {
+        this.languageLevels.push(languageLevel);
+    }
+
+    public updateLanguageLevel(languageLevel: LanguageLevel) {
+        const index = this.languageLevels.findIndex((level) => level.getLanguageId() === languageLevel.getLanguageId());
+        this.languageLevels[index] = languageLevel;
+    }
+
+    public resetLanguageLevels() {
+        this.languageLevels = [];
+        this.addOverallLevel();
+    }
+
+    private addOverallLevel() {
+        this.languageLevels.unshift(new LanguageLevel('overall'));
+    }
+}
