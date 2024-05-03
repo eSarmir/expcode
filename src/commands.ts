d285c253cd279d43a13697a341f2a87e27495639
Erik Šarmír <e.sarmir@gmail.com>
2024-05-03 20:44:08 +0200

    Cleans up the commands (#29)

--- src/commands.ts
+++ src/commands.ts
@@ -13,40 +13,9 @@ export function registerCommands(
 
     const disposables: vscode.Disposable[] = [];
 
-    disposables.push(showLanguageOfActiveEditor());
-
-    disposables.push(showLevelForLanguage(languageLevels));
-
     disposables.push(resetProgress(context, languageLevels));
 
     return disposables;
-}
-
-function showLanguageOfActiveEditor(): vscode.Disposable {
-    return vscode.commands.registerCommand(ShowLanguageOfActiveEditorCommand, () => {
-		
-        let currentLanguage = vscode.window.activeTextEditor?.document.languageId;
-
-        vscode.window.showInformationMessage(`Current Language: ${currentLanguage}`);
-    });
-}
-
-function showLevelForLanguage(languageLevels: LanguageLevel[]) {
-    return vscode.commands.registerCommand(ShowLevelForLanguageCommand, () => {
-
-        let currentLanguage = vscode.window.activeTextEditor?.document.languageId;
-	
-		let languageLevel = languageLevels.find(
-			(languageLevel) => languageLevel.getLanguageId() === currentLanguage);
-
-        if (languageLevel === undefined) {
-            return;
-        }
-
-        vscode.window.showInformationMessage(
-            languageLevel.stringify()
-        );
-    });
 }
 
 function resetProgress(
